98ea301ec0e58661c47ad502417adae1fdbc2884
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2023-05-27 16:06:53 -0400

    search scenes query

---
+++ components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row py-2 d-flex justify-content-center">
+        <div class="col-lg-6">
+            <div class="search">
+                <input v-model="searching" class="input-search" type="search" :placeholder="searching" required>
+                <button class="btn-search" @click="SearchQuery(searching)">Search</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+let searching = 'Search here ...';
+
+const SearchQuery = (_search) => {
+    console.log(_search);
+    router.push({ path: '/search/' + searching + '/1' })
+}
+</script>
--- pages/search/[query]/[page].vue
+++ pages/search/[query]/[page].vue
@@ -1,26 +1,18 @@
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <NuxtLink to="/">Home</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/scenes">Scenes</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/posts/2">Post 2</NuxtLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
     <div>
         <h1>Search page :{{ page }} - query :{{ query }}</h1>
     </div>
+    <div> {{ searchData }} </div>
 </template>
 
 <script setup>
 const route = useRoute();
 let page = route.params.page;
 let query = route.params.query;
+
+const { isMobile, isTablet } = useDevice();
+
+const { data: searchData } = await useFetch('https://jav.souzou.dev/search?title=' + query + '&page=' + page);
+
+
 </script>
