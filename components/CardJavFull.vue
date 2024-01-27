a356532d7c40a1746d25fd31213526d0a3c33394
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2024-01-27 15:48:16 -0300

    update jav and video

---
+++ components/CardJavFull.vue
@@ -0,0 +1,29 @@
+<template>
+    <NuxtLink :to="'/javs/jav/' + props.data.code" class="movie my-2">
+        <div class="movie-hero">
+            <img :src="props.data.poster" class="movie-img">
+        </div>
+        <div class="movie-content">
+            <div class="movie-title">
+                <h1 class="heading-primary">{{ props.data.code }}</h1>
+            </div>
+            <div class="movie-title">
+                <div class="row">
+                    <div v-for="category in props.data.categories" :key="category.id" class="col-lg-4 text-center"><div class="my-1 movie-tag movie-tag-1">{{ category.name }}</div></div>
+                </div>                
+            </div>
+            <p class="movie-description">
+                {{ props.data.title }}
+            </p>
+            <div class="movie-title">
+                <div class="row">
+                    <div v-for="idol in props.data.idols" :key="idol.id" class="col-lg-12 text-center"><div class="my-1 movie-tag movie-tag-idol">{{ idol.name }}</div></div>
+                </div>                
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps(['data']);
+</script>
--- pages/javs/jav/[code].vue
+++ pages/javs/jav/[code].vue
@@ -1,50 +1,26 @@
 <template>
-    <div class="container-fluid">
-        <div class="row row-title my-2 py-1">
-            <div class="col-lg-12 text-center">
-                <h6>{{ getJav.Jav.title }}</h6>
+    <div class="container">
+        <div class="row my-2 py-1">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="title">{{ javData.code + " - " + javData.title }}</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <CardJavFull v-bind:data="javData" />
+                </div>
             </div>
         </div>
-        <div class="container container-scene">
+        <div class="row my-2 py-1">
             <div class="row">
-                <div class="col-lg-12 px-0">
-                    <video id="jav_player">
-                        <source :src="getJav.Jav.video" title='720p' type="video/mp4" />
-                        <source :src="getJav.Jav.video" title='480p' type="video/mp4" />
-                    </video>
+                <div class="col-lg-12">
+                    <h3 class="title">Videos from this JAV</h3>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 ps-0">
-                    <NuxtLink :to="'/javs/jav/' + getJav.Jav.code">
-                        <img :src="getJav.Jav.poster" />
-                    </NuxtLink>
-                </div>
-                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                    <div class="row">
-                        <p class="info">{{ getJav.Jav.code }} - {{ getJav.Jav.title }}</p>
-                    </div>
-                    <div class="row">
-                        <p class="info">Categories</p>
-                    </div>
-                    <div class="row">
-                        <div v-for="category in getJav.Categories" :key="category.id"
-                            v-bind:class="getColumnsCategoriesIdols()">
-                            <NuxtLink :to="'/categories/' + category.name + '/1'" class="info-categories">
-                                <p>{{ category.name }}</p>
-                            </NuxtLink>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <p class="info">Idols</p>
-                    </div>
-                    <div class="row">
-                        <div v-for="idol in getJav.Idols" :key="idol.id" v-bind:class="getColumnsCategoriesIdols()">
-                            <NuxtLink :to="'/idols/' + idol.name + '/1'" class="info-idols">
-                                <p>{{ idol.name }}</p>
-                            </NuxtLink>
-                        </div>
-                    </div>
+                <div v-for="scene in javData.videos" :key="scene.id" class="col-lg-3">
+                    <CardScene v-bind:data="scene" />
                 </div>
             </div>
         </div>
@@ -53,7 +29,6 @@
 
 <script setup>
 const route = useRoute();
-const { isMobile, isTablet } = useDevice();
 let code = route.params.code;
 
 const runtimeConfig = useRuntimeConfig();
@@ -63,66 +38,26 @@ if (!code || code.trim().length === 0) {
     throw createError({ statusCode: 500, statusMessage: 'It seems that you are using invalid parameters!' })
 }
 
-if (isMobile) {
-    limitRelated = 4
-}
-
 const { data: getJav } = await useFetch(api + '/javs/getjavbycode?code=' + code);
-if (!getJav._rawValue.Jav) {
+
+if (!getJav._value.Response) {
     console.log('NULL Jav');
     throw createError({ statusCode: 404, statusMessage: 'You found a dead end!' })
 }
 
-const view = await useFetch(api + '/javs/getJavViewById?id=' + getJav._rawValue.Jav.id);
+let javData = getJav._value.Response;
 
 useHead({
-    title: getJav._rawValue.Jav.code + " | Jav4Free | " + getJav._rawValue.Jav.title,
+    title: javData.code + " | Jav4Free | " + javData.title,
     meta: [
         {
             name: 'description', content: "Jav4Free, watch " +
-                getJav._rawValue.Jav.code +
+                javData.code +
                 " , " +
-                getJav._rawValue.Jav.title +
+                javData.title +
                 " , Here you can find almost every Idol and Actress of japanese adult videos, find the latest japanese adult videos in high quality, various Idols and categories. Every video stream quickly and with amazing quality."
         }
     ]
 })
-
-onMounted(() => {
-    var player = fluidPlayer('jav_player', {
-        layoutControls: {
-            layout: "default",
-            fillToContainer: false,
-            preload: true,
-            posterImage: getJav._rawValue.Jav.poster,
-            timelinePreview: {
-                file: getJav._rawValue.Jav.vtt,
-                type: "VTT",
-            },
-            allowTheatre: false,
-            contextMenu: {
-                controls: false
-            }
-        },
-    });
-
-});
-
-const getColumnsScenes = () => {
-    if (isTablet) {
-        return 'col-lg-6 col-md-6 col-sm-6 col-xs-6'
-    } else {
-        return 'col-lg-3 col-md-3 col-sm-3 col-xs-3'
-    }
-};
-
-const getColumnsCategoriesIdols = () => {
-    if (isTablet || isMobile) {
-        return 'col-lg-4 col-md-4 col-sm-4 col-xs-4'
-    } else {
-        return 'col-lg-2 col-md-2 col-sm-2 col-xs-2'
-    }
-};
-
 
 </script>
