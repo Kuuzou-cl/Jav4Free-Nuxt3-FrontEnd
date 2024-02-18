c89b087a4ccf7aa8c599bba06516762f8b30041a
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2024-02-17 23:33:11 -0300

    new upload page

--- pages/dashboard/cloudflare-images/new/index.vue
+++ pages/dashboard/cloudflare-images/new/index.vue
@@ -11,7 +11,26 @@
                     <label class="title">Select file</label>
                 </div>
                 <div class="row mb-4">
-                    <input class="form-control form-control-sm" ref="formFile" type="file" @change="onFileChange()">
+                    <input class="form-control form-control-sm" ref="formFile" type="file">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <label class="title">Variants Created</label>
+                </div>
+                <div class="row mb-4">
+                    <table class="table table-bordered table-admin" v-if="renderComponent">
+                        <thead>
+                            <tr>
+                                <th>Url</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="url in variantsURL">
+                                <th>{{ url }}</th>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -37,45 +56,53 @@ definePageMeta({
 });
 
 const runtimeConfig = useRuntimeConfig();
-const token = runtimeConfig.public.apiTokenCloudflare;
+const api = runtimeConfig.public.apiBase;
 
 const formFile = ref(0);
+const renderComponent = ref(true);
 
-let fileToUpload;
+let variantsURL = [];
 
 
 const postImage = async () => {
-    const cookieBearer = 'Bearer ' + token;
-    const myHeaders = new Headers();    
-    myHeaders.append("Authorization", cookieBearer);
-    myHeaders.append("Content-Type", "multipart/form-data");
+    const cookieToken = useCookie('token');
+    const cookieBearer = 'Bearer ' + cookieToken.value;
+    const myHeaders = new Headers();
+    myHeaders.append("authorization", cookieBearer);
 
-    const formData = new FormData();
-
-    formData.append("file", formFile._value.files[0], formFile._value.files[0].name);
-    formData.append("requireSignedURLs",false);
-
-    var requestOptions = {
+    const { data: getURL } = await useFetch(api + '/javs/generateUploadUrl', {
         method: 'POST',
-        headers: myHeaders,
-        body: formData,
-        redirect: 'follow'
+        headers: myHeaders
+    })
+
+    console.log(getURL._value.Response.result.uploadURL);
+
+    if (!getURL._value.Response) {
+        throw createError({ statusCode: 404, statusMessage: 'You found a dead end!' })
+    }
+    let urlToUpload = getURL._value.Response.result.uploadURL;
+
+    const formdata = new FormData();
+    formdata.append("file", formFile._value.files[0], formFile._value.files[0].name);
+
+    const requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow"
     };
 
-    await fetch("https://api.cloudflare.com/client/v4/accounts/70e8c8aff115acf6bcc8cd9998cdda6e/images/v1", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    console.log(urlToUpload)
 
+    renderComponent.value = false;
 
-};
+    const { data: getURLImage } = await useFetch(urlToUpload, requestOptions);
 
+    variantsURL = getURLImage._value.result.variants;
 
-const onFileChange = () => {
-    console.log(formFile._value.files[0].name);
-    console.log(formFile._value.files[0]);
-    fileToUpload = formFile._value.files[0];
-    console.log(fileToUpload);
+    console.log(variantsURL);
+    console.log(getURLImage._value.result.variants);
+    renderComponent.value = true;
 }
+
 
 </script>
