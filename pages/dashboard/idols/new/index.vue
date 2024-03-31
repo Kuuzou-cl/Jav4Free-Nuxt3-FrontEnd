9cbd85c27d6f6ceab43c9c092bad37c4c7d2e3b3
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2024-03-31 18:49:19 -0300

    new admin page new idol

--- pages/dashboard/idols/new/index.vue
+++ pages/dashboard/idols/new/index.vue
@@ -1,45 +1,88 @@
 <template>
-    <div class="container-fluid">
-        <div class="row row-title my-2 py-1">
-            <div class="col-lg-12 text-center">
-                <h6>New Idol</h6>
+    <div class="container">
+        <div class="row my-2 py-1">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="title">New Idol</h3>
+                </div>
             </div>
         </div>
-        <div class="container">
-            <div class="row my-4">
-                <div class="col-lg-6 ">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <label class="title-input-admin">Idol Name</label>
-                            <input v-model="newIdolName" class="input-admin"
-                                placeholder="Enter Idol name" />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <label class="title-input-admin">Hidden</label>
-                            <input type="checkbox" class="input-admin" v-model="newIdolHide" />
-                        </div>
-                    </div>
+        <div class="row my-4">
+            <div class="col-lg-3">
+                <div class="row">
+                    <label class="title">Select file</label>
                 </div>
-                <div class="col-lg-6 d-flex justify-content-center">
-                    <div class="row">
-                        <div class="col-lg-12 d-flex justify-content-center">
-                            <img class="preview-img-jav" ref="imgCodePreview"
-                                :src="imgPreview" />
-                        </div>
-                        <button class="btn btn-warning" @click="changeUrlImg()">
-                            Reload Cover
+                <div class="row mb-4">
+                    <input class="form-control form-control-sm" ref="formFile" type="file">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <label class="title">Variants Created</label>
+                </div>
+                <div class="row mb-4">
+                    <table class="table table-bordered table-admin" v-if="renderComponent">
+                        <thead>
+                            <tr>
+                                <th>Url</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="url in variantsURL">
+                                <th>{{ url }}</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="col-lg-12 d-flex justify-content-center">
+                        <button class="btn btn-success" @click="postImage()">
+                            Upload Image
                         </button>
                     </div>
                 </div>
             </div>
-            <div class="row my-2">
-                <div class="col-lg-12 d-flex justify-content-center">
-                    <button class="btn btn-success" @click="postIdol()">
-                        Add Idol
+        </div>
+        <div class="row my-4">
+            <div class="col-lg-6 ">
+                <div class="row">
+                    <label class="title">Idol Name</label>
+                </div>
+                <div class="row mb-4">
+                    <input v-model="newIdolName" class="input-admin" placeholder="Enter Idol name" />
+                </div>
+                <div class="row">
+                    <label class="title">Image link</label>
+                </div>
+                <div class="row mb-4">
+                    <input v-model="newIdolImage" class="input-admin" placeholder="Enter cloudflare image link" />
+                </div>
+            </div>
+            <div class="col-lg-6 ">
+                <div class="row justify-content-center mb-4">
+                    <button class="btn category-title" @click="changeUrlImg()">
+                        Reload Files
                     </button>
                 </div>
+                <div class="row">
+                    <div class="col-lg-6 text-center">
+                        <div class="row">
+                            <h6 class="title">Image</h6>
+                        </div>
+                        <div class="row justify-content-center mb-4">
+                            <img class="preview-img-jav" ref="imgCodePreview" :src="imgPreview" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-lg-12 d-flex justify-content-center">
+                <button class="btn btn-success" @click="postIdol()">
+                    New Idol
+                </button>
             </div>
         </div>
     </div>
@@ -55,15 +98,23 @@ definePageMeta({
     layout: "admin",
 });
 
+const runtimeConfig = useRuntimeConfig();
+const api = runtimeConfig.public.apiBase;
+
+const formFile = ref(0);
+const renderComponent = ref(true);
+
+let variantsURL = [];
+
 const imgCodePreview = ref(0);
 
 let newIdolName = "";
-let newIdolHide = true;
+let newIdolImage = "";
 
 let imgPreview = "";
 
 const changeUrlImg = async () => {
-    imgPreview = "https://d27vxor1f495av.cloudfront.net/idols/" + newIdolName.toLowerCase().replace(" ", "-") + ".jpg";
+    imgPreview = newIdolImage;
     imgCodePreview._rawValue.src = imgPreview;
 }
 
@@ -73,16 +124,59 @@ const postIdol = async () => {
     const myHeaders = new Headers();
     myHeaders.append("authorization", cookieBearer);
 
-    const { data, error } = await useFetch('https://jav.souzou.dev/idols/newIdolv2', {
+    const { data, error } = await useFetch(api + '/idols/newIdol', {
         method: 'POST',
         headers: myHeaders,
         body: {
             name: newIdolName,
-            image: imgPreview,
-            hide: newIdolHide            
+            poster: newIdolImage
         }
     })
 
+    reloadNuxtApp({
+        path: "/dashboard/idols/1",
+        ttl: 1000, // default 10000
+    });
 };
+
+const postImage = async () => {
+    const cookieToken = useCookie('token');
+    const cookieBearer = 'Bearer ' + cookieToken.value;
+    const myHeaders = new Headers();
+    myHeaders.append("authorization", cookieBearer);
+
+    const { data: getURL } = await useFetch(api + '/javs/generateUploadUrl', {
+        method: 'POST',
+        headers: myHeaders
+    })
+
+    console.log(getURL._value.Response.result.uploadURL);
+
+    if (!getURL._value.Response) {
+        throw createError({ statusCode: 404, statusMessage: 'You found a dead end!' })
+    }
+    let urlToUpload = getURL._value.Response.result.uploadURL;
+
+    const formdata = new FormData();
+    formdata.append("file", formFile._value.files[0], formFile._value.files[0].name);
+
+    const requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow"
+    };
+
+    console.log(urlToUpload)
+
+    renderComponent.value = false;
+
+    const { data: getURLImage } = await useFetch(urlToUpload, requestOptions);
+
+    variantsURL = getURLImage._value.result.variants;
+
+    console.log(variantsURL);
+    console.log(getURLImage._value.result.variants);
+    renderComponent.value = true;
+}
 
 </script>
